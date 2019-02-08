491623df5c5838b66e83c0d26d643b43789cd46a
Md. Mostak Shahid <mostak.shaid@gmail.com>
2019-02-08 16:54:17 +0600

    update

---
+++ gallery/admin/includes/database.php
@@ -0,0 +1,16 @@
+<?php
+require_once('new_config.php');
+class Database {
+	private $connection;	
+	// function __construct(argument) {
+	// 	# code...
+	// }
+	public open_db_connection () {
+		$this -> connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+		if (mysqli_connect_errno()){
+			die("Database connection failed." . mysqli_error());
+		}
+	}
+}
+$database = new Database();
+$database -> open_db_connection ();
---
+++ gallery/admin/includes/new_config.php
@@ -0,0 +1,6 @@
+<?php
+// Database Connection Constants
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_NAME','oop_gallery');
